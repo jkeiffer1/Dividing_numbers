d029ecc6c4b934190a356396712f0fc6e459d129
jkeiffer1 <saros82@hotmail.com>
2017-07-05 13:18:00 -0400

    added 1 test for 1 doesn't equal 2

--- test_divide.rb
+++ test_divide.rb
@@ -6,6 +6,9 @@ class Division_test < Minitest::Test
 	def test_1_equals_1
 		assert_equal(1,1)
 	end
+	def test_1_no_eql_2
+		assert_equal(1,2)
+	end
 
 	def test_8_divided_2_equals_4
 		assert_equal(4, divide(8,2))
@@ -17,6 +20,13 @@ class Division_test < Minitest::Test
 
 	def test_0_divide_2
 		assert_equal(0, divide(0,2))
-		
+	end
+
+	def test_using_x_and_y
+		x = 0
+		y = 2
+		assert_equal(0, divide(x,y))
 	end
 end
+
+
